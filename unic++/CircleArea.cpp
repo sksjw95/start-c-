11612533a879be11721bcacef2e8f7993e610c53
sksjw95 <tjwldnd95@hanmail.net>
2023-09-04 21:08:48 +0900

    학교공부6

---
+++ unic++/CircleArea.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	const double PI{ 3.14159 }; // 원주율 정의
+	double radius; // 원의 반지름
+
+	cout << "원의 반경을 입력하시오: ";
+	cin >> radius; // 반지름 입력
+	double area = radius * radius * PI; // 넓이 계산
+	cout << "원의 면적 = " << area << endl; // 결과 출력
+}
