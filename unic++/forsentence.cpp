ed2aab1a718eb8e1f399bcdd23b8d41d9b61ae75
sksjw95 <tjwldnd95@hanmail.net>
2023-09-09 01:57:44 +0900

    학교공부8

---
+++ unic++/forsentence.cpp
@@ -0,0 +1,12 @@
+/*#include<iostream>
+using namespace std;
+
+int main() {
+	int val, total = 0;
+	cin >> val;
+	for (int i = 0; i < 10; i++) {
+		//cin >> val; -> 여기에 위치하면 for 문이 실행될 때마다 val 값을 입력해야 한다.
+		total += val;
+	}
+	cout << total;
+}*/
