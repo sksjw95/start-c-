9344ac983d10a8e3c54b5431365e55c0b3035122
sksjw95 <tjwldnd95@hanmail.net>
2023-09-10 14:47:36 +0900

    학교공부9

---
+++ unic++/enhancedforsentence.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sum = 0;
+	for (int a : arr) {
+		sum += a;
+	}
+	cout << "гу╟Х = " << sum << endl;
+}
