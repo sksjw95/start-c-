7a6ea248b60c1fe28c5de2b37966e74e6b6fa67f
sksjw95 <tjwldnd95@hanmail.net>
2023-09-11 00:03:48 +0900

    학교공부10

---
+++ unic++/enhancedforsentence2.cpp
@@ -0,0 +1,14 @@
+/*#include<iostream>
+using namespace std;
+//int sum;
+int main() {
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sum = 0 ;
+	
+	for ( int a: arr) {
+		sum+= ++a ;// -> arr배열에는 영향이 없다. a만 증가한 것이다.
+
+	}
+	cout << sum<< endl;
+	cout << arr[0]<<arr[1] << endl;
+}*/
