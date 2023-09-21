fcb376527bdd23ae14954abb60bdc8e84344eab1
sksjw95 <tjwldnd95@hanmail.net>
2023-09-22 01:57:41 +0900

    학교공부18

---
+++ unic++/MaxValue.cpp
@@ -0,0 +1,19 @@
+/*#include<iostream>
+using namespace std;
+
+int main() {
+	int max;
+	int data[10] = { 10,23,5,9,22,48,12,10,55,31 };
+
+	max = data[0];// data 인덱스 0번의 값을 max로 지정
+	cout << "데이터 : " << data[0]; // 0번 데이터 출력
+	for (int i = 1; i < 10; i++) {
+		cout << " " << data[i];
+		if (max < data[i]) {
+			max = data[i];
+		}
+	}
+	cout << "\n\n배열의 최댓값 : " << max << endl;
+	return 0;
+
+}*/
